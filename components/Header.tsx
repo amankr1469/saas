52e82d55c8299dd87c1340a69002e2c5e69ead00
Aman Kumar <akjha1469@gmail.com>
2023-11-14 18:34:04 +0530

    header updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,15 @@
+import { getServerSession } from "next-auth"
 import { DarkModeToggle } from "./DarkModeToggle"
 import Logo from "./Logo"
 import UserButton from "./UserButton"
+import { authOptions } from "@/auth"
+import Link from "next/link"
+import { MessageSquareIcon } from "lucide-react"
 
-function Header() {
+async function Header() {
+
+  const session = await getServerSession(authOptions);
+
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto" >
@@ -10,11 +17,20 @@ function Header() {
 
         <div className="flex-1 flex items-center justify-end space-x-4">
 
-            {/*DARK MODE BUTTON*/}
-            <DarkModeToggle />
+          {session ? (
+            <>
+              <Link href={"/chat"} prefetch={false}>
+                <MessageSquareIcon className="text-black dark:text-white"/>              
+              </Link>
+            </>
+          ) : (
+            <>
+            <Link href='/pricing'>Pricing</Link>
+            </>
+          )}
 
-            {/*DARK MODE BUTTON*/}
-            <UserButton />
+          <DarkModeToggle />
+          <UserButton />
         </div>
       </nav>
     </header>
